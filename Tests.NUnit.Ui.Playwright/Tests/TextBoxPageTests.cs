7d9cadbd5c71022e0b5cee2bb9df04edf182b5d9
Oleh Kutafin <kutafin.o.v@gmail.com>
2024-10-21 16:35:03 -0700

    first comit

---
+++ Tests.NUnit.Ui.Playwright/Tests/TextBoxPageTests.cs
@@ -0,0 +1,8 @@
+namespace Tests.NUnit.Ui.Playwright.Tests;
+
+//TODO: Add tests
+[TestFixture]
+public class TextBoxPageTests
+{
+    
+}
---
+++ Tests.xUnit.Ui.Playwright/Tests/ButtonsPageTests.cs
@@ -0,0 +1,7 @@
+namespace Tests.xUnit.Ui.Playwright.Tests;
+
+//TODO: Add tests
+public class ButtonsPageTests
+{
+    
+}
