3d59ba1a6e8b7df10a738c7c78d311859761d06e
Liudmyla <savinska.l.y@gmail.com>
2024-10-22 18:41:18 +0200

    Added ButtonsTests

--- Tests.NUnit.Ui.Playwright/Tests/ButtonsPageTests.cs
+++ Tests.NUnit.Ui.Playwright/Tests/ButtonsPageTests.cs
@@ -4,10 +4,11 @@ using static Microsoft.Playwright.Playwright;
 namespace Tests.NUnit.Ui.Playwright.Tests;
 
 [TestFixture]
-public class ButtonsPageTests
+public class ButtonsPageTests: PageTest
 { private IBrowser Browser { get; set; }
     private IBrowserContext Context { get; set; }
     private IPage Page { get; set; }
+    
     
     [SetUp]
     public async Task Setup()
@@ -35,6 +36,49 @@ public class ButtonsPageTests
         Page.SetDefaultTimeout(10000);
         await Page.GotoAsync("https://demoqa.com/buttons");
     }
+    
+    [Test]
+    public async Task GoToButtonsPage_TitleIsCorrect()
+    {
+        var title = await Page.TextContentAsync("h1");
 
-
+        Assert.That(title, Is.EqualTo("Buttons"));
+    }
+    
+    [Test]
+    public async Task DoubleClickButton_ReturnsMessage()
+    {
+        await Page.DblClickAsync("xpath=/html//button[@id='doubleClickBtn']");
+        var doubleClickMessage = await Page.Locator("xpath=/html//p[@id='doubleClickMessage']")
+            .TextContentAsync();
+        
+        Assert.That(doubleClickMessage, Is.EqualTo("You have done a double click"));
+    }
+    
+    [Test]
+    public async Task RightClickButton_ReturnsMessage()
+    {
+        await Page.GetByText("Right Click Me").ClickAsync(new LocatorClickOptions { Button = MouseButton.Right });
+        var rightClickMessage = await Page.Locator("xpath=/html//p[@id='rightClickMessage']")
+            .TextContentAsync();
+        
+        Assert.That(rightClickMessage, Is.EqualTo("You have done a right click"));
+    }
+    
+    [Test]
+    public async Task ClickButton_ReturnsMessage()
+    {
+        await Page.ClickAsync("xpath=/html//button[@id='doubleClickBtn']");
+        var clickMessage = await Page.Locator("xpath=/html//p[@id='dynamicClickMessage']")
+            .TextContentAsync();
+        
+        Assert.That(clickMessage, Is.EqualTo("You have done a dynamic click"));
+    }
+    
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        await Page.CloseAsync();
+        await Browser.CloseAsync();
+    }
 }
