be2acf72865950bea82b04687f4da27f8fde6625
Liudmyla <savinska.l.y@gmail.com>
2024-10-22 15:24:18 +0200

    Added TextBoxTests

--- Tests.NUnit.Ui.Playwright/Tests/TextBoxPageTests.cs
+++ Tests.NUnit.Ui.Playwright/Tests/TextBoxPageTests.cs
@@ -10,7 +10,7 @@ public class TextBoxPageTests : PageTest
     private IBrowserContext Context { get; set; }
     private IPage Page { get; set; }
     
-    [SetUp]
+    [OneTimeSetUp]
     public async Task Setup()
     {
         using var playwright = await CreateAsync();
@@ -38,8 +38,62 @@ public class TextBoxPageTests : PageTest
     }
 
     [Test]
-    public async Task OpenTextBoxPage_TitleIsCorrect()
+    public async Task GoToTextBoxPage_TitleIsCorrect()
     {
-        
+        var title = await Page.TextContentAsync("h1");
+
+        Assert.That(title, Is.EqualTo("Text Box"));
+    }
+    
+    [Test]
+    public async Task CheckCorrectLabelsAreDisplayed()
+    {
+        var textBoxLabelName = await Page.TextContentAsync("#userName-label");
+        var textBoxLabelEmail = await Page.TextContentAsync("#userEmail-label");
+        var textBoxLabelCurrentAddress = await Page.TextContentAsync("currentAddress-label");
+        var textBoxLabelPermanentAddress = await Page.TextContentAsync("#permanentAddress-label");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(textBoxLabelName, Is.EqualTo("Full Name"));
+            Assert.That(textBoxLabelEmail, Is.EqualTo("Email"));
+            Assert.That(textBoxLabelCurrentAddress, Is.EqualTo("Current Address"));
+            Assert.That(textBoxLabelPermanentAddress, Is.EqualTo("Permanent Address"));
+        });
+    }
+    
+    [Test]
+    public async Task CompleteTheForm_EnteredDataIsDisplayed()
+    {
+       //id selector
+       await Page.Locator("id=userName").FillAsync("Liudmyla Savinska");
+       //css selector
+       await Page.Locator("#userEmail").FillAsync("liudatest@test.com");
+       await Page.FillAsync("#currentAddress", "200 Crescent Ave, Covington, KY 41011, United States");
+       //xpath selector
+       await Page.FillAsync("xpath=//*[@id='permanentAddress']", "6834 Hollywood Blvd\\nLos Angeles, California 90028-6116");
+       
+       await Page.GetByText("Submit").ClickAsync();
+
+        var textToAssert = await Page.Locator("xpath=//div[@id='output']/div[@class='border col-md-12 col-sm-12']")
+            .TextContentAsync();
+
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(textToAssert, Is.Not.Null);
+            Assert.That(textToAssert, Is.Not.Empty);
+            Assert.That(textToAssert!.Contains("Liudmyla Savinska"));
+            Assert.That(textToAssert.Contains("liudatest@test.com"));
+            Assert.That(textToAssert.Contains("200 Crescent Ave, Covington, KY 41011, United States"));
+            Assert.That(textToAssert.Contains("6834 Hollywood Blvd\\nLos Angeles, California 90028-6116"));
+        });
+    }
+    
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        await Page.CloseAsync();
+        await Browser.CloseAsync();
     }
 }
