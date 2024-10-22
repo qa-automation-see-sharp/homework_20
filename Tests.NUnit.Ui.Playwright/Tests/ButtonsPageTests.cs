71a57113d7e3c143116316c1388256e44a9d6113
Liudmyla <savinska.l.y@gmail.com>
2024-10-22 15:03:39 +0200

    Added set up for the tests

--- Tests.NUnit.Ui.Playwright/Tests/ButtonsPageTests.cs
+++ Tests.NUnit.Ui.Playwright/Tests/ButtonsPageTests.cs
@@ -1,8 +1,40 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using static Microsoft.Playwright.Playwright;
 namespace Tests.NUnit.Ui.Playwright.Tests;
 
-//TODO: Add tests
 [TestFixture]
 public class ButtonsPageTests
-{
+{ private IBrowser Browser { get; set; }
+    private IBrowserContext Context { get; set; }
+    private IPage Page { get; set; }
     
+    [SetUp]
+    public async Task Setup()
+    {
+        using var playwright = await CreateAsync();
+        playwright.Selectors.SetTestIdAttribute("aria-label");
+        
+        Browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+            SlowMo = 50,
+            Timeout = 10000,
+            Args = ["--start-maximized"],
+            
+        });
+        
+        Context = await Browser.NewContextAsync(new BrowserNewContextOptions
+        {
+            ViewportSize = ViewportSize.NoViewport,
+            Locale = "en-US",
+            ColorScheme = ColorScheme.NoPreference,
+        });
+
+        Page = await Context.NewPageAsync();
+        Page.SetDefaultTimeout(10000);
+        await Page.GotoAsync("https://demoqa.com/buttons");
+    }
+
+
 }
