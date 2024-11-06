bc8cd00a029c7374b9e6a8194d670979e35fdc86
AntonPavliuchyk <AntonPavliuchyk@L2LGC-LPT44>
2024-11-06 21:57:22 +0200

    20 homework is ready

--- Tests.NUnit.Ui.Playwright/Tests/ButtonsPageTests.cs
+++ Tests.NUnit.Ui.Playwright/Tests/ButtonsPageTests.cs
@@ -1,8 +1,106 @@
+using Microsoft.Playwright;
+using static Microsoft.Playwright.Playwright;
+
 namespace Tests.NUnit.Ui.Playwright.Tests;
 
 //TODO: Add tests
 [TestFixture]
 public class ButtonsPageTests
 {
-    
+    private IBrowser Browser { get; set; }
+    private IBrowserContext Context { get; set; }
+    private IPage Page { get; set; }
+
+    [SetUp]
+    public async Task Setup()
+    {
+        var playwright = await CreateAsync();
+
+        Browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+            SlowMo = 50,
+            Timeout = 10000,
+            Args = ["--start-maximized"]
+        });
+
+        Context = await Browser.NewContextAsync(new BrowserNewContextOptions
+        {
+            ViewportSize = ViewportSize.NoViewport,
+            Locale = "en-US",
+            ColorScheme = ColorScheme.NoPreference
+        });
+
+        Page = await Context.NewPageAsync();
+
+        Page.SetDefaultTimeout(10000);
+        await Page.GotoAsync("https://demoqa.com/buttons");
+    }
+
+    [Test]
+    public async Task DoubleClickButtonTest()
+    {
+        var isButtonDisplayed = await Page.Locator("id=doubleClickBtn").IsVisibleAsync();
+        var isButtonEnabled = await Page.Locator("id=doubleClickBtn").IsEnabledAsync();
+        await Page.Locator("id=doubleClickBtn").DblClickAsync();
+
+        var outputDoubleClick = "You have done a double click";
+        var textOutput = await Page.Locator("id=doubleClickMessage").TextContentAsync();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(isButtonDisplayed, Is.True);
+            Assert.That(isButtonEnabled, Is.True);
+            Assert.That(textOutput, Is.Not.Null);
+            Assert.That(textOutput, Is.Not.Empty);
+            Assert.That(textOutput, Does.Contain(outputDoubleClick));
+        });
+    }
+
+    [Test]
+    public async Task RightClickButtonTest()
+    {
+        var isButtonDisplayed = await Page.Locator("id=rightClickBtn").IsVisibleAsync();
+        var isButtonEnabled = await Page.Locator("id=rightClickBtn").IsEnabledAsync();
+        await Page.Locator("id=rightClickBtn").ClickAsync(new() { Button = MouseButton.Right });
+
+        var outputRightClick = "You have done a right click";
+        var textOutput = await Page.Locator("id=rightClickMessage").TextContentAsync();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(isButtonDisplayed, Is.True);
+            Assert.That(isButtonEnabled, Is.True);
+            Assert.That(textOutput, Is.Not.Null);
+            Assert.That(textOutput, Is.Not.Empty);
+            Assert.That(textOutput, Does.Contain(outputRightClick));
+        });
+    }
+
+    [Test]
+    public async Task LeftClickButtonTest()
+    {
+        var isButtonDisplayed = await Page.Locator("xpath=//button[text()='Click Me']").IsVisibleAsync();
+        var isButtonEnabled = await Page.Locator("xpath=//button[text()='Click Me']").IsEnabledAsync();
+        await Page.Locator("xpath=//button[text()='Click Me']").ClickAsync();
+
+        var outputLeftClick = "You have done a dynamic click";
+        var textOutput = await Page.Locator("id=dynamicClickMessage").TextContentAsync();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(isButtonDisplayed, Is.True);
+            Assert.That(isButtonEnabled, Is.True);
+            Assert.That(textOutput, Is.Not.Null);
+            Assert.That(textOutput, Is.Not.Empty);
+            Assert.That(textOutput, Does.Contain(outputLeftClick));
+        });
+    }
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        await Page.CloseAsync();
+        await Browser.CloseAsync();
+    }
 }
---
+++ Tests.NUnit.Ui.Playwright/Tests/RadioButtonPageTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using static Microsoft.Playwright.Playwright;
+
+namespace Tests.NUnit.Ui.Playwright.Tests
+{    
+    [TestFixture]
+
+    public class RadioButtonPageTests
+    {
+        private IBrowser Browser { get; set; }
+        private IBrowserContext Context { get; set; }
+        private IPage Page { get; set; }
+
+        [SetUp]
+        public async Task Setup()
+        {
+            var playwright = await CreateAsync();
+
+            Browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                SlowMo = 50,
+                Timeout = 10000,
+                Args = ["--start-maximized"]
+            });
+
+            Context = await Browser.NewContextAsync(new BrowserNewContextOptions
+            {
+                ViewportSize = ViewportSize.NoViewport,
+                Locale = "en-US",
+                ColorScheme = ColorScheme.NoPreference
+            });
+
+            Page = await Context.NewPageAsync();
+
+            Page.SetDefaultTimeout(10000);
+            await Page.GotoAsync("https://demoqa.com/radio-button");
+        }
+
+        [Test]
+        public async Task RadioButtonYes()
+        {
+            await Page.Locator("label.custom-control-label[for='yesRadio']").ClickAsync();
+            var isButtonSelected = await Page.Locator("id=yesRadio").IsCheckedAsync();
+            var isButtonEnabled = await Page.Locator("id=yesRadio").IsEnabledAsync();
+
+            var outputSelection = "You have selected";
+            var outputYes = "Yes";
+            var isOutputDisplayed = await Page.Locator(".mt-3").IsVisibleAsync();
+            var isSuccessMessageDisplayed = await Page.Locator(".text-success").IsVisibleAsync();
+            var output = await Page.Locator(".mt-3").TextContentAsync();
+            var successMessage = await Page.Locator(".text-success").TextContentAsync();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(isButtonSelected, Is.True);
+                Assert.That(isButtonEnabled, Is.True);
+                Assert.That(isOutputDisplayed, Is.True);
+                Assert.That(isSuccessMessageDisplayed, Is.True);
+                Assert.That(output, Does.Contain(outputSelection));
+                Assert.That(successMessage, Does.Contain(outputYes));
+            });
+        }
+
+        [Test]
+        public async Task RadioButtonImpressive()
+        {
+            await Page.Locator("label.custom-control-label[for='impressiveRadio']").ClickAsync();
+            var isButtonSelected = await Page.Locator("id=impressiveRadio").IsCheckedAsync();
+            var isButtonEnabled = await Page.Locator("id=impressiveRadio").IsEnabledAsync();
+
+            var outputSelection = "You have selected";
+            var outputImpresive = "Impressive";
+            var isOutputDisplayed = await Page.Locator(".mt-3").IsVisibleAsync();
+            var isSuccessMessageDisplayed = await Page.Locator(".text-success").IsVisibleAsync();
+            var output = await Page.Locator(".mt-3").TextContentAsync();
+            var successMessage = await Page.Locator(".text-success").TextContentAsync();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(isButtonSelected, Is.True);
+                Assert.That(isButtonEnabled, Is.True);
+                Assert.That(isOutputDisplayed, Is.True);
+                Assert.That(isSuccessMessageDisplayed, Is.True);
+                Assert.That(output, Does.Contain(outputSelection));
+                Assert.That(successMessage, Does.Contain(outputImpresive));
+            });
+        }
+
+        [Test]
+        public async Task RadioButtonNo()
+        {
+            var isButtonEnabled = await Page.Locator("id=noRadio").IsEnabledAsync();
+
+            Assert.That(isButtonEnabled, Is.False);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await Page.CloseAsync();
+            await Browser.CloseAsync();
+        }
+    }
+}
